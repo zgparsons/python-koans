19c795020e3b16b0c1748c4ea58e1e291d876457
zgparsons <z.g.parsons@outlook.com>
2020-11-30 10:59:18 +0000

    about_dice_project.py

--- python3/koans/about_dice_project.py
+++ python3/koans/about_dice_project.py
@@ -14,9 +14,10 @@ class DiceSet:
         return self._values
 
     def roll(self, n):
-        # Needs implementing!
-        # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = list()
+        for roll in range(n):
+            self._values.append(random.randint(1, 6))
+        return self._values
 
 class AboutDiceProject(Koan):
     def test_can_create_a_dice_set(self):
